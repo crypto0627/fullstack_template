e13325e67163acbc7af7cfe843fc6865a40bbacd
crypto0627 <jake0627a1@gmail.com>
2024-04-06 15:41:14 +0800

    feat: wagmi change

--- apps/web/src/components/layout.tsx
+++ apps/web/src/components/layout.tsx
@@ -2,9 +2,24 @@
 
 import React from "react";
 import { ThemeProvider } from "@material-tailwind/react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from '../wagmi.config'
+
+const queryClient = new QueryClient()
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider><div>{children}</div></ThemeProvider>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <div>
+            {children}
+          </div>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
 
 export default Layout;
